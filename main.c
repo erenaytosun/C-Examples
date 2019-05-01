472d4132ffae41af038e72785a83d78cb7298430
Erenay Tosun <erenaytosun77@gmail.com>
2019-05-01 20:42:37 +0300

    Simple_Math_Program

--- main.c
+++ main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-int main()
-{
+int menu(){
+
     int secim=0;
-    while(secim!=1){
+    while(secim!=1 && secim!=2){
 
-        printf("1-Oyuna Basla <1>\n2-Oyunu Bitir <-1>\n Secim Gir : ");
+        printf("1-Asal sayi bulma \n2-Sayi carpimi\n3- Cikis \n ");
         scanf("%d",&secim);
         if(secim==1){
-            secim=0;
-            fonksiyon();
+            int asal;
+            printf("Sayi gir: \n");
+            scanf("%d",&asal);
+            int yol=asal/2;
+            int gelen=asalbul(asal,yol);
+            if(gelen==1){
+                printf("%d asal sayidir\n",asal);
+            }
+            else{
+                printf("Asal sayi degildir\n");
+            }
+            return secim;
         }
-        else if(secim==-1){
+
+        else if(secim==2){
+
+            srand(time(NULL));
+            int sayi1=1+rand()%20;
+            int sayi2=2+rand()%99;
+            printf("Sayi 1: %d    Sayi 2: %d\n",sayi1,sayi2);
+            int sonuc=carpim(sayi1,sayi2);
+            printf("Sonuc : %d\n",sonuc);
+            return secim;
+        }
+
+        else if(secim==3){
             break;
         }
         else{
@@ -20,27 +43,41 @@ int main()
         }
     }
 
-
-
-    return 0;
 }
-void fonksiyon(){
 
-    int sayi=10;
-    int tahmin=0;
+int asalbul(int sayi,int yari){
 
-    while(tahmin!=sayi){
-        printf("Tahmininizi giriniz : "); scanf("%d",&tahmin);
-        if(tahmin==sayi){
-            printf("Tebrikler Bildiniz..\n");
-        }
-        else if(tahmin<sayi){
-            printf("Daha buyuk bir sayi giriniz..\n");
-        }
-        else{
-            printf("Daha kucuk bir sayi giriniz..\n");
-        }
+    if(sayi<2){
+
+        return 0;
+    }
+    if(yari==2){
+
+        return 1;
+    }
+    if(sayi%yari==0){
+        return 0;
+
+    }
+    asalbul(sayi,yari-1);
+
+}
+int carpim(int ilk,int son){
+
+    if(ilk==0 || son==0){
+        return 0;
+    }
+    return ilk+carpim(ilk,son-1);
+
+}
+
+int main()
+{
+    int secim=1;
+    while(secim==1 || secim==2){
+        secim=menu();
     }
 
-
+    printf("Program sonlandi...");
+    return 0;
 }
